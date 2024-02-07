f399288c18009c8c755f8b85d9da7fb659f15cc5
ConcreteShield <sondinand@gmail.com>
2024-02-07 12:42:00 +0100

    Added Health System

---
+++ Assets/Scripts/Dinand Scripts/HealthSystem/HealthBarHUDTester.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class HealthBarHUDTester : MonoBehaviour
+{
+    public void Heal(float health)
+    {
+        PlayerStats.Instance.Heal(health);
+    }
+
+    public void Hurt(float dmg)
+    {
+        PlayerStats.Instance.TakeDamage(dmg);
+    }
+}
