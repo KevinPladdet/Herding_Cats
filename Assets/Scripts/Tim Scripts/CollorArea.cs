2fedb7246ecf7a486a36c1d19d69f8db0b87a288
timpan42 <tim@fagerdal.se>
2024-02-07 08:50:57 +0100

    f*ck up, not sending .vscode files no more

--- Assets/Scripts/Tim Scripts/CollorArea.cs
+++ Assets/Scripts/Tim Scripts/CollorArea.cs
@@ -7,49 +7,60 @@ public class CollorArea : MonoBehaviour
 {
     [SerializeField] private String collorOfArea;
 
-    private void OnCollisionEnter(Collision cat) {
+    private void OnCollisionEnter(Collision cat)
+    {
         // Check object name
-        if(cat.transform.name.Contains(collorOfArea)){
+        if (cat.transform.name.Contains(collorOfArea))
+        {
             DeletCatObject(cat);
             //DisableCatObject(cat);
         }
-        else{
+        else
+        {
             TheWrongCat();
         }
-        
-        // Check objects script varible name 
-        if(cat.transform.GetComponent<CatScript>().catCollarName == collorOfArea){
+
+        // Check objects script variable name 
+        if (cat.transform.GetComponent<CatScript>().catCollarName == collorOfArea)
+        {
             TheRightCat(cat);
         }
-        else{
+        else
+        {
             TheWrongCat();
         }
 
         // Check objects paret name 
-        if(cat.transform.parent.gameObject.name.Contains(collorOfArea)){
+        if (cat.transform.parent.gameObject.name.Contains(collorOfArea))
+        {
             TheRightCat(cat);
         }
-        else{
-            TheWrongCat();   
+        else
+        {
+            TheWrongCat();
         }
     }
 
-    private void TheRightCat(Collision cat){
+    private void TheRightCat(Collision cat)
+    {
         Debug.Log("this is " + collorOfArea + " cat");
         DeletCatObject(cat);
-    }   
+    }
 
-    private void TheWrongCat(){
-        Debug.Log("not the right cat"); 
+    private void TheWrongCat()
+    {
+        Debug.Log("not the right cat");
     }
 
     //If you want to delet object
-    private void DeletCatObject(Collision cat){
-          Destroy(cat.gameObject);    
+    private void DeletCatObject(Collision cat)
+    {
+        Destroy(cat.gameObject);
     }
 
     //If you want to disable object
-    private void DisableCatObject(Collision cat){
-          cat.gameObject.SetActive(false);  
+    private void DisableCatObject(Collision cat)
+    {
+        cat.gameObject.SetActive(false);
     }
 }
